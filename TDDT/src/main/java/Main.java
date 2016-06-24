e30e0c9aeb6e8e884757bfa03a32586586d40641
Andreas Burbach <andreas.burbach@uni-duesseldorf.de>
2016-06-24 15:02:45 +0200

    Master (#4)
    * Initial Commit
    
    Projekt erstellt.
    
    * Protokoll erstellt
    
    Hochladen des Protokolls der erstellen Woche des Projekts 7 des
    Programmierpraktikums.
    
    * 1. Protokoll aktualisiert
    
    Nachnahme von Antonio fehlte
    
    * Wird neu gemacht
    
    * Gradle Projekt erstellt
    
    Code bitte in
    programmierpraktikum-abschlussprojekt-tuck-festing\TDDT\src\main\java
    
    * Test
    
    * Nochmal Gradle Pullen

---
+++ TDDT/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andreas on 24.06.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello Tuck Festing!");
+    }
+
+
+}
